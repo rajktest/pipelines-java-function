ed7481a805498ef291d6e2851b91aae2f0678de9
rajktest <54575526+rajktest@users.noreply.github.com>
2019-08-29 14:50:02 +0100

    Update FunctionTest.java

--- src/test/java/com/functionp/FunctionTest.java
+++ src/test/java/com/functionp/FunctionTest.java
@@ -53,6 +53,6 @@ public class FunctionTest {
         final HttpResponseMessage ret = new Function().run(req, context);
 
         // Verify
-        assertEquals(ret.getStatus(), HttpStatus.OK+1);
+        assertEquals(ret.getStatus(), HttpStatus.ACCEPTED);
     }
 }
